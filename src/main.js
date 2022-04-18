0d667eb29857f25e0be477d2f714e34242adeef5
Andrew <andrewmgunn26@gmail.com>
2022-04-18 13:24:26 -0500

    bootstrap-vue added to import, bootstrap css added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/scss/bootstrap.scss'; // npm install node-sass
+import {BootstrapVue} from "bootstrap-vue";
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import {firebase} from "@/firebase";
 
+Vue.use(BootstrapVue)
 createApp(App).mount('#app')
